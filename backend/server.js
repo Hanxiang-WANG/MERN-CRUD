5ebc77d32f6c6d866fc30b40d69eb5424b17b798
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2025-01-05 22:35:18 +1100

    post api update

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,22 @@ dotenv.config();
 
 const app = express();
 
-app.post("/products", async (req, res) => {
+app.post("/api/products", async (req, res) => {
   const product  = req.body; //user will send this data
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success:false, message:"Please provide all fields" });
+  }
+
+  const newProduct = new product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () =>{
