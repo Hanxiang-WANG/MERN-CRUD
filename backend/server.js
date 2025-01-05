eb3eb2f9acf5c822ddfca1bf867a6e70e4243077
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2025-01-06 10:33:03 +1100

    delete product

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js"
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json()); //allows us to accept JSON data in the body
 
 app.post("/api/products", async (req, res) => {
   const product  = req.body; //user will send this data
@@ -13,7 +16,7 @@ app.post("/api/products", async (req, res) => {
     return res.status(400).json({ success:false, message:"Please provide all fields" });
   }
 
-  const newProduct = new product(product);
+  const newProduct = new Product(product);
 
   try {
     await newProduct.save();
@@ -24,7 +27,17 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product Deleted" });
+  } catch (error) {
+    
+  }
+});
+
 app.listen(5000, () =>{
     connectDB();
-    console.log('Server started at http://localhost:5000 hello');
+    console.log('Server started at http://localhost:5000');
 });
