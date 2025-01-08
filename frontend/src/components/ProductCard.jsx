e27911ddc97f9a2abe756c48042462d24bca030f
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2025-01-08 16:16:17 +1100

    home page products fetch

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Button, Image, Text } from '@chakra-ui/react';
+
+const ProductCard = () => {
+    return (
+        <Box shadow='lg' rounded='lg' overflow='hidden' transition='all 0.3s' _hover={{ transform: 'translateY(-5px)', shadow: 'xl' }}>
+
+        </Box>
+    )
+}
+
+export default ProductCard
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useProductStore } from '@/store/product';
+import { use } from 'react';
 
 const HomePage = () => {
+    const { fetchProducts, products } = useProductStore();
+
+    useEffect(() => {
+        fetchProducts();
+    }, [fetchProducts]);
+    console.log(products);
+
     return (
-        <div>HomePage</div>
+        <Container maxW='container.xl' py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={'bold'}
+                    textAlign={'center'}
+                    bgGradient={"linear(to-r, #000000, #e74c3c)"}
+                //bgClip={'text'}
+                >
+                    Current Products
+                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w='full'>
+
+                </SimpleGrid>
+
+                <Text fontSize="xl" textAlign={'center'} fontWeight={'bold'} color='gray.500'>
+                    No products found{' '}
+                    <Link to={'/create'}>
+                        <Text as='span' color='blue.500' _hover={{ textDecoration: 'underline' }}>
+                            Create a new product!
+                        </Text>
+                    </Link>
+                </Text>
+            </VStack>
+        </Container>
     )
 }
 
